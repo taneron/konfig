42caf31422039547f73331c6a3f04329f598a108
dphuang2 <dylan.p.huang@gmail.com>
2023-04-30 14:48:24 -0700

    rm-submodule works

--- scripts/rm-submodule.sh
+++ scripts/rm-submodule.sh
@@ -13,17 +13,22 @@ path=$1
 # remove trailing slash from path
 path=${path%/}
 
-# remove the submodule from the index
-git submodule deinit -f $path
+# preserve files
 
-# remove the submodule from the .gitmodules file
-git config -f .gitmodules --remove-section submodule.$path
+cp -r $path ${path}_tmp
 
 # remove the submodule from the working directory
-git rm --cached $path
+git rm $path
+
+# remove evidence of git for that submodule
+rm ${path}_tmp/.git
+rm -rf .git/modules/${path}
+
+# move preserved files back
+mv ${path}_tmp $path
 
 # remove the submodule from the project files but keep the files
 git add $path
 
 # commit the changes
-git commit -m "Removed submodule: $path"
+# git commit -m "Removed submodule: $path"
