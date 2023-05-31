a7951d80b631b9be46b48d6f6f7454be1cdce1b0
dphuang2 <dylan.p.huang@gmail.com>
2023-05-31 01:57:54 -0700

    fix dependency cycle

--- generator/konfig-dash/packages/konfig-lib/src/md-to-demo-content.test.ts
+++ generator/konfig-dash/packages/konfig-lib/src/md-to-demo-content.test.ts
@@ -1,9 +1,9 @@
-import { mdToDemoContent } from './md-to-demo-content'
+// import { mdToDemoContent } from './md-to-demo-content'
 
 describe('md-to-demo-content', () => {
   it.skip('simple', async () => {
-    const test = await mdToDemoContent({
-      markdown: '# Test `clientId`\n\n```python\nprint("test")```',
-    })
+    // const test = await mdToDemoContent({
+    //   markdown: '# Test `clientId`\n\n```python\nprint("test")```',
+    // })
   })
 })
