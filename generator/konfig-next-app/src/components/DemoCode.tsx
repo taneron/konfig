92b97c7c53a888900bfaaefd7e5f4e7d0e559757
dphuang2 <dylan.p.huang@gmail.com>
2023-06-09 02:39:04 -0700

    fix build

--- generator/konfig-next-app/src/components/DemoCode.tsx
+++ generator/konfig-next-app/src/components/DemoCode.tsx
@@ -1,16 +1,16 @@
-import { Box, Code, Collapse, Transition } from '@mantine/core'
-import { Prism } from '@mantine/prism'
-import { observer } from 'mobx-react'
-import { useContext, useEffect, useState } from 'react'
-import { Components } from 'react-markdown'
-import { DemoStateContext } from './DemoMarkdown'
+import { Box, Code, Collapse, Transition } from "@mantine/core";
+import { Prism } from "@mantine/prism";
+import { observer } from "mobx-react";
+import { useContext, useEffect, useState } from "react";
+import { Components } from "react-markdown";
+import { DemoStateContext } from "./DemoMarkdown";
 
 const langDisplayName = {
-  python: 'Python',
-  typescript: 'TypeScript',
-} as const
+  python: "Python",
+  typescript: "TypeScript",
+} as const;
 
-const _DemoCode: Components['code'] = ({
+const _DemoCode: Components["code"] = ({
   node,
   inline,
   className,
@@ -18,22 +18,23 @@ const _DemoCode: Components['code'] = ({
   siblingCount,
   ...props
 }) => {
-  if (inline) return <Code>{children}</Code>
-  const match = /language-(\w+)/.exec(className || '')
+  const demoState = useContext(DemoStateContext);
+
+  if (inline) return <Code>{children}</Code>;
+  const match = /language-(\w+)/.exec(className || "");
   if (match === null || inline)
     return (
       <code {...props} className={className}>
         {children}
       </code>
-    )
-  const language = match[1]
-  if (language !== 'python' && language !== 'typescript')
+    );
+  const language = match[1];
+  if (language !== "python" && language !== "typescript")
     return (
       <code {...props} className={className}>
         {children}
       </code>
-    )
-  const demoState = useContext(DemoStateContext)
+    );
 
   return (
     <Transition
@@ -52,14 +53,14 @@ const _DemoCode: Components['code'] = ({
                 </Prism.Tab>
               </Prism.TabsList>
               <Prism.Panel value={language} language={language}>
-                {String(children).replace(/\n$/, '')}
+                {String(children).replace(/\n$/, "")}
               </Prism.Panel>
             </Prism.Tabs>
           </div>
-        )
+        );
       }}
     </Transition>
-  )
-}
+  );
+};
 
-export const DemoCode = observer(_DemoCode)
+export const DemoCode = observer(_DemoCode);
--- generator/konfig-next-app/src/components/DemoForm.tsx
+++ generator/konfig-next-app/src/components/DemoForm.tsx
@@ -1,58 +1,58 @@
-import { api } from '@/utils/api'
-import { LocalVariables, LocalVariablesType } from '@/utils/schemas'
-import { Stack } from '@mantine/core'
-import { createFormContext } from '@mantine/form'
-import { observer } from 'mobx-react'
-import { useState, useContext, createContext, useEffect } from 'react'
-import { Components } from 'react-markdown'
-import { DemoState, DemoStateContext } from './DemoMarkdown'
-import { makeAutoObservable } from 'mobx'
-import { Position } from 'unist-util-position/lib'
-import { v4 as uuid } from 'uuid'
-import dayjs from 'dayjs'
+import { api } from "@/utils/api";
+import { LocalVariables, LocalVariablesType } from "@/utils/schemas";
+import { Stack } from "@mantine/core";
+import { createFormContext } from "@mantine/form";
+import { observer } from "mobx-react";
+import { useState, useContext, createContext, useEffect } from "react";
+import { Components } from "react-markdown";
+import { DemoState, DemoStateContext } from "./DemoMarkdown";
+import { makeAutoObservable } from "mobx";
+import { Position } from "unist-util-position/lib";
+import { v4 as uuid } from "uuid";
+import dayjs from "dayjs";
 
 export const FormContext = createContext<
   ReturnType<typeof createFormContext>[1] | null
->(null)
+>(null);
 
-export const CellContext = createContext<CellState | null>(null)
+export const CellContext = createContext<CellState | null>(null);
 
-type RunState = 'Success' | 'Error' | 'N/A'
+type RunState = "Success" | "Error" | "N/A";
 export class CellState {
-  show = false
-  running = false
-  runState: RunState = 'N/A'
-  output: string = ''
-  demoState: DemoState | null
-  id = uuid()
+  show = false;
+  running = false;
+  runState: RunState = "N/A";
+  output: string = "";
+  demoState: DemoState | null;
+  id = uuid();
 
   constructor({ demoState }: { demoState: DemoState | null }) {
-    makeAutoObservable(this)
-    this.demoState = demoState
+    makeAutoObservable(this);
+    this.demoState = demoState;
   }
 
   get ranSuccessfully() {
-    return this.runState === 'Success'
+    return this.runState === "Success";
   }
 
   get failed() {
-    return this.runState === 'Error'
+    return this.runState === "Error";
   }
 
   setRunning(value: boolean) {
-    this.running = value
+    this.running = value;
   }
 
   setShow(value: boolean) {
-    this.show = value
+    this.show = value;
   }
 
   setOutput(value: string) {
-    this.output = value
+    this.output = value;
   }
 
   setRunState(value: RunState) {
-    this.runState = value
+    this.runState = value;
   }
 
   async run({
@@ -60,13 +60,13 @@ export class CellState {
     environmentVariables,
     localVariables,
   }: {
-    position: Position
-    environmentVariables?: unknown
-    localVariables: unknown
+    position: Position;
+    environmentVariables?: unknown;
+    localVariables: unknown;
   }) {
-    if (!this.demoState?.sessionId) return
-    this.setRunning(true)
-    this.setShow(false)
+    if (!this.demoState?.sessionId) return;
+    this.setRunning(true);
+    this.setShow(false);
 
     const response = await api.executeCode.query({
       organizationId: this.demoState.portal.organizationId,
@@ -75,27 +75,27 @@ export class CellState {
       sessionId: this.demoState?.sessionId,
       codePosition: position,
       localVariables: LocalVariables.parse(localVariables),
-    })
+    });
 
-    this.setRunning(false)
+    this.setRunning(false);
 
-    if (response.result === 'Could not find demo') {
-      this.setRunState('Error')
-      return
+    if (response.result === "Could not find demo") {
+      this.setRunState("Error");
+      return;
     }
 
-    if (response.result === 'Could not find code') {
-      this.setRunState('Error')
-      return
+    if (response.result === "Could not find code") {
+      this.setRunState("Error");
+      return;
     }
-    this.setOutput(response.output)
-    this.setRunning(false)
-    this.setShow(true)
-    this.setRunState(response.error === '' ? 'Success' : 'Error')
+    this.setOutput(response.output);
+    this.setRunning(false);
+    this.setShow(true);
+    this.setRunState(response.error === "" ? "Success" : "Error");
   }
 }
 
-const _Form: Components['form'] = ({
+const _Form: Components["form"] = ({
   node,
   children,
   siblingCount,
@@ -103,138 +103,138 @@ const _Form: Components['form'] = ({
 }) => {
   const [[FormProvider, useFormContext, useForm]] = useState(
     createFormContext()
-  )
+  );
 
   // collect all optional inputs
   const optionals = node.children.reduce((optionals, child) => {
-    if (child.type === 'element') {
-      const name = child.properties?.['name']
-      const optional = child.properties?.['optional']
+    if (child.type === "element") {
+      const name = child.properties?.["name"];
+      const optional = child.properties?.["optional"];
       if (
-        typeof name === 'string' &&
+        typeof name === "string" &&
         optional !== undefined &&
         optional !== null
       ) {
-        optionals.push(name)
+        optionals.push(name);
       }
     }
-    return optionals
-  }, [] as string[])
+    return optionals;
+  }, [] as string[]);
 
   // collect all precision attributes formats from number inputs
   const precisions = node.children.reduce((precisions, child) => {
-    if (child.type === 'element' && child.tagName === 'number') {
-      const name = child.properties?.['name']
-      const precision = child.properties?.['precision']
-      if (typeof name === 'string' && typeof precision === 'string') {
-        precisions[name] = parseInt(precision)
+    if (child.type === "element" && child.tagName === "number") {
+      const name = child.properties?.["name"];
+      const precision = child.properties?.["precision"];
+      if (typeof name === "string" && typeof precision === "string") {
+        precisions[name] = parseInt(precision);
       }
     }
-    return precisions
-  }, {} as { [key: string]: number })
+    return precisions;
+  }, {} as { [key: string]: number });
 
   // collect any value formats from date inputs
   const valueFormats = node.children.reduce((valueFormats, child) => {
-    if (child.type === 'element' && child.tagName === 'date') {
-      const name = child.properties?.['name']
-      const valueFormat = child.properties?.['valueFormat']
-      if (typeof name === 'string' && typeof valueFormat === 'string') {
-        valueFormats[name] = valueFormat
+    if (child.type === "element" && child.tagName === "date") {
+      const name = child.properties?.["name"];
+      const valueFormat = child.properties?.["valueFormat"];
+      if (typeof name === "string" && typeof valueFormat === "string") {
+        valueFormats[name] = valueFormat;
       }
     }
-    return valueFormats
-  }, {} as { [key: string]: string })
+    return valueFormats;
+  }, {} as { [key: string]: string });
 
   // Initialize values
   const initialValues = node.children.reduce((initialValues, child) => {
-    if (child.type === 'element') {
-      if (child.tagName === 'input') {
-        const name = child.properties?.['name']
-        const type = child.properties?.['type']
-        const defaultValue = child.properties?.['defaultValue']
-        if (typeof name === 'string') {
+    if (child.type === "element") {
+      if (child.tagName === "input") {
+        const name = child.properties?.["name"];
+        const type = child.properties?.["type"];
+        const defaultValue = child.properties?.["defaultValue"];
+        if (typeof name === "string") {
           initialValues[name] =
-            type === 'checkbox'
-              ? defaultValue === 'true'
-              : defaultValue !== undefined && typeof defaultValue === 'string'
+            type === "checkbox"
+              ? defaultValue === "true"
+              : defaultValue !== undefined && typeof defaultValue === "string"
               ? defaultValue
-              : ''
+              : "";
         }
-      } else if (child.tagName === 'number') {
-        const name = child.properties?.['name']
-        const defaultValue = child.properties?.['defaultValue']
-        if (typeof name === 'string') {
+      } else if (child.tagName === "number") {
+        const name = child.properties?.["name"];
+        const defaultValue = child.properties?.["defaultValue"];
+        if (typeof name === "string") {
           initialValues[name] =
-            typeof defaultValue === 'string' ? parseFloat(defaultValue) : ''
+            typeof defaultValue === "string" ? parseFloat(defaultValue) : "";
         }
       }
     }
-    return initialValues
-  }, {} as { [key: string]: string | boolean | number })
+    return initialValues;
+  }, {} as { [key: string]: string | boolean | number });
 
   // Ensure all non-optional inputs are non-empty
   const validate = node.children.reduce((validate, child) => {
-    if (child.type === 'element' && child.tagName === 'input') {
-      const name = child.properties?.['name']
-      const label = child.properties?.['label']
-      const optional = child.properties?.['optional']
-      if (typeof name === 'string' && optional === undefined) {
+    if (child.type === "element" && child.tagName === "input") {
+      const name = child.properties?.["name"];
+      const label = child.properties?.["label"];
+      const optional = child.properties?.["optional"];
+      if (typeof name === "string" && optional === undefined) {
         validate[name] = (value) =>
-          value === '' ? `${label} is required` : null
+          value === "" ? `${label} is required` : null;
       }
     }
-    return validate
-  }, {} as { [key: string]: (value: string) => string | null })
+    return validate;
+  }, {} as { [key: string]: (value: string) => string | null });
 
   // Find the first section of code that is a Python snippet
   const firstPreNode = node.children.find((child) => {
-    return child.type === 'element' && child.tagName === 'pre'
-  })
+    return child.type === "element" && child.tagName === "pre";
+  });
 
-  const form = useForm({ initialValues, validate })
+  const form = useForm({ initialValues, validate });
 
   useEffect(() => {
     node.children.forEach((child) => {
-      if (child.type === 'element') {
-        if (child.tagName === 'input') {
-          const name = child.properties?.['name']
-          if (typeof name === 'string') {
-            const stored = localStorage.getItem(name)
-            if (stored !== null && stored !== 'null') {
-              if (stored === 'true') form.setFieldValue(name, true)
-              else if (stored === 'false') form.setFieldValue(name, false)
-              else form.setFieldValue(name, stored)
+      if (child.type === "element") {
+        if (child.tagName === "input") {
+          const name = child.properties?.["name"];
+          if (typeof name === "string") {
+            const stored = localStorage.getItem(name);
+            if (stored !== null && stored !== "null") {
+              if (stored === "true") form.setFieldValue(name, true);
+              else if (stored === "false") form.setFieldValue(name, false);
+              else form.setFieldValue(name, stored);
             }
           }
-        } else if (child.tagName === 'date') {
-          const name = child.properties?.['name']
-          if (typeof name === 'string') {
-            const stored = localStorage.getItem(name)
+        } else if (child.tagName === "date") {
+          const name = child.properties?.["name"];
+          if (typeof name === "string") {
+            const stored = localStorage.getItem(name);
             if (
               stored !== null &&
-              stored !== 'null' &&
+              stored !== "null" &&
               !isNaN(new Date(stored).getTime())
             ) {
-              form.setFieldValue(name, new Date(stored))
+              form.setFieldValue(name, new Date(stored));
             }
           }
         }
       }
-      return initialValues
-    })
-  }, [])
+      return initialValues;
+    });
+  }, [form, initialValues, node.children]);
 
-  const demoState = useContext(DemoStateContext)
+  const demoState = useContext(DemoStateContext);
 
   // Turns out if you want to do something once for a component this is the
   // pattern you should follow to actually ensure that child components can
   // react to state updates
-  const [cell] = useState(new CellState({ demoState }))
+  const [cell] = useState(new CellState({ demoState }));
   useEffect(() => {
-    if (demoState === null) return
-    if (demoState.cells.includes(cell)) return
-    demoState.pushCell({ cell })
-  }, [])
+    if (demoState === null) return;
+    if (demoState.cells.includes(cell)) return;
+    demoState.pushCell({ cell });
+  }, [cell, demoState]);
 
   return (
     <FormContext.Provider value={useFormContext}>
@@ -243,67 +243,67 @@ const _Form: Components['form'] = ({
           <form
             onSubmit={form.onSubmit(async (values) => {
               if (demoState === null) {
-                return
+                return;
               }
               if (demoState === null) {
-                return
+                return;
               }
               if (firstPreNode === undefined) {
-                return
+                return;
               }
               if (firstPreNode.position === undefined) {
-                return
+                return;
               }
 
               // copy existing values object to new object
-              const newValues: LocalVariablesType = {}
+              const newValues: LocalVariablesType = {};
               for (const [key, value] of Object.entries(values as object)) {
-                newValues[key] = value
+                newValues[key] = value;
               }
 
               // convert precision floats to BoxedFloats based on "precision" attribute
               for (const [key, value] of Object.entries(values as object)) {
-                if (!(typeof value === 'number')) continue
-                if (!(key in precisions)) continue
+                if (!(typeof value === "number")) continue;
+                if (!(key in precisions)) continue;
                 newValues[key] = {
-                  type: 'float',
+                  type: "float",
                   data: value.toFixed(precisions[key]),
                   precision: precisions[key],
-                }
+                };
               }
 
               // convert Dates to strings based on "valueFormat" attribute
               for (const [key, value] of Object.entries(values as object)) {
-                if (!(value instanceof Date)) continue
+                if (!(value instanceof Date)) continue;
                 if (!(key in valueFormats)) {
-                  newValues[key] = value.toISOString()
-                  continue
+                  newValues[key] = value.toISOString();
+                  continue;
                 }
-                newValues[key] = dayjs(value).format(valueFormats[key])
+                newValues[key] = dayjs(value).format(valueFormats[key]);
               }
 
               // remove null values
               for (const [key, value] of Object.entries(values as object)) {
                 if (value === null) {
-                  delete newValues[key]
+                  delete newValues[key];
                 }
               }
 
               // remove optional empty string values
               for (const [key, value] of Object.entries(values as object)) {
-                if (value === '' && optionals.includes(key)) {
-                  delete newValues[key]
+                if (value === "" && optionals.includes(key)) {
+                  delete newValues[key];
                 }
               }
 
               await cell?.run({
                 position: firstPreNode.position,
                 localVariables: newValues,
-              })
+              });
 
               if (cell?.ranSuccessfully) {
                 for (const [key, value] of Object.entries(values as object)) {
-                  localStorage.setItem(key, value)
+                  localStorage.setItem(key, value);
                 }
               }
             })}
@@ -314,7 +314,7 @@ const _Form: Components['form'] = ({
         </FormProvider>
       </CellContext.Provider>
     </FormContext.Provider>
-  )
-}
+  );
+};
 
-export const DemoForm = observer(_Form)
+export const DemoForm = observer(_Form);
