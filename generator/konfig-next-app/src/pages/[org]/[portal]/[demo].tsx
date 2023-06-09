a9327c055e8dd36ce729ab21607f616ac9192d62
dphuang2 <dylan.p.huang@gmail.com>
2023-06-09 09:36:50 -0700

    getStaticProps

--- generator/konfig-next-app/src/pages/[org]/[portal]/[demo].tsx
+++ generator/konfig-next-app/src/pages/[org]/[portal]/[demo].tsx
@@ -1,54 +1,61 @@
-import { DemoPortal, PortalState } from '@/components/DemoPortal'
-import { observer } from 'mobx-react'
-import { Organization, Portal, Demo, demos } from '@/utils/demos'
-import Head from 'next/head'
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
-import { useState } from 'react'
+import { DemoPortal, PortalState } from "@/components/DemoPortal";
+import { observer } from "mobx-react";
+import { Organization, Portal, Demo, demos } from "@/utils/demos";
+import Head from "next/head";
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import { useState } from "react";
 
-export const getServerSideProps: GetServerSideProps<{
-  organization: Organization
-  portal: Portal
-  demo: Demo
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps<{
+  organization: Organization;
+  portal: Portal;
+  demo: Demo;
 }> = async (ctx) => {
   if (!ctx.params?.org || Array.isArray(ctx.params.org)) {
     return {
       notFound: true,
-    }
+    };
   }
-  const organization = demos.find(({ id }) => id === ctx.params?.org)
+  const organization = demos.find(({ id }) => id === ctx.params?.org);
 
   if (organization === undefined)
     return {
       notFound: true,
-    }
+    };
 
   if (!ctx.params?.portal || Array.isArray(ctx.params.portal)) {
     return {
       notFound: true,
-    }
+    };
   }
 
   const portal = organization.portals.find(
     ({ id }) => id === ctx.params?.portal
-  )
+  );
 
   if (portal === undefined)
     return {
       notFound: true,
-    }
+    };
 
   if (!ctx.params?.demo || Array.isArray(ctx.params.demo)) {
     return {
       notFound: true,
-    }
+    };
   }
 
-  const demo = portal.demos.find(({ id }) => id === ctx.params?.demo)
+  const demo = portal.demos.find(({ id }) => id === ctx.params?.demo);
 
   if (demo === undefined)
     return {
       notFound: true,
-    }
+    };
 
   return {
     props: {
@@ -56,22 +63,22 @@ export const getServerSideProps: GetServerSideProps<{
       portal,
       demo,
     },
-  }
-}
+  };
+};
 
 const Snaptrade = observer(
   ({
     organization,
     portal,
     demo,
-  }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  }: InferGetStaticPropsType<typeof getStaticProps>) => {
     const [state] = useState(
       new PortalState({
         ...portal,
         organizationId: organization.id,
         demoId: demo.id,
       })
-    )
+    );
 
     return (
       <>
@@ -80,8 +87,8 @@ const Snaptrade = observer(
         </Head>
         <DemoPortal state={state} />
       </>
-    )
+    );
   }
-)
+);
 
-export default Snaptrade
+export default Snaptrade;
