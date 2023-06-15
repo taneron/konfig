8ccc1acade8d4ed75c0525e39f66b2e7b6fd5fb7
dphuang2 <dylan.p.huang@gmail.com>
2023-06-15 01:15:34 -0700

    TODO: sandbox dev mode

--- generator/konfig-next-app/src/pages/sandbox.tsx
+++ generator/konfig-next-app/src/pages/sandbox.tsx
@@ -1,7 +1,9 @@
-import { Container, SegmentedControl } from "@mantine/core";
+import { Button, Container } from "@mantine/core";
 import { observer } from "mobx-react";
 import Head from "next/head";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { useEffect, useState } from "react";
+import { watch } from "fs";
 
 /**
  * This is here to force this page to be SSR only so Next.js doesn't try to make
@@ -12,13 +14,43 @@ export const getServerSideProps: GetServerSideProps<{}> =
     return { props: {} };
   };
 
+async function watchDirectoryChanges() {
+  try {
+    const directoryHandle = await (window as any).showDirectoryPicker();
+
+    const watcher = directoryHandle.watch();
+
+    watcher.addEventListener("change", (event: any) => {
+      for (const change of event.changes) {
+        if (change.kind === "added") {
+          console.log(`File added: ${change.file.name}`);
+        } else if (change.kind === "removed") {
+          console.log(`File removed: ${change.file.name}`);
+        } else if (change.kind === "updated") {
+          console.log(`File updated: ${change.file.name}`);
+        }
+      }
+    });
+
+    watcher.addEventListener("error", (error: any) => {
+      console.error(`Watcher error: ${error}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 const MarkdownSandboxPage = observer(() => {
   return (
     <>
       <Head>
         <title>Demo Sandbox | Konfig</title>
       </Head>
-      <Container pt="xl" size="lg"></Container>
+      <Container pt="xl" size="lg">
+        <Button onClick={async () => await watchDirectoryChanges()}>
+          Specify demo.yaml
+        </Button>
+      </Container>
     </>
   );
 });
