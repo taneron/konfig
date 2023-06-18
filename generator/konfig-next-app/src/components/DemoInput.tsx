18f065db633e02f8de8f956439327338bc63f366
dphuang2 <dylan.p.huang@gmail.com>
2023-06-17 17:49:09 -0700

    remove withAsterisk for checkbox

--- generator/konfig-next-app/src/components/DemoInput.tsx
+++ generator/konfig-next-app/src/components/DemoInput.tsx
@@ -1,10 +1,10 @@
-import { Checkbox, PasswordInput, TextInput } from '@mantine/core'
-import { useContext } from 'react'
-import { Components } from 'react-markdown'
-import { FormContext } from './DemoForm'
-import { observer } from 'mobx-react'
+import { Checkbox, PasswordInput, TextInput } from "@mantine/core";
+import { useContext } from "react";
+import { Components } from "react-markdown";
+import { FormContext } from "./DemoForm";
+import { observer } from "mobx-react";
 
-const _DemoInput: Components['input'] = ({
+const _DemoInput: Components["input"] = ({
   node,
   children,
   size,
@@ -13,27 +13,27 @@ const _DemoInput: Components['input'] = ({
   defaultValue,
   ...props
 }) => {
-  let Component = TextInput
-  if (type === 'password') Component = PasswordInput
-  if (type === 'checkbox') Component = Checkbox as any
-  const useFormContext = useContext(FormContext)
-  const form = useFormContext?.()
-  const name = node.properties?.name
+  let Component = TextInput;
+  if (type === "password") Component = PasswordInput;
+  if (type === "checkbox") Component = Checkbox as any;
+  const useFormContext = useContext(FormContext);
+  const form = useFormContext?.();
+  const name = node.properties?.name;
   const inputProps =
-    form !== undefined && typeof name === 'string'
+    form !== undefined && typeof name === "string"
       ? form.getInputProps(name, {
-          type: type === 'checkbox' ? 'checkbox' : 'input',
+          type: type === "checkbox" ? "checkbox" : "input",
         })
-      : undefined
-  const optional = node.properties?.optional !== undefined
+      : undefined;
+  const optional = node.properties?.optional !== undefined;
   return (
     <Component
-      withAsterisk={!optional ? true : undefined}
+      withAsterisk={type !== "checkbox" && !optional ? true : undefined}
       autoComplete="off"
       {...props}
       {...inputProps}
     />
-  )
-}
+  );
+};
 
-export const DemoInput = observer(_DemoInput)
+export const DemoInput = observer(_DemoInput);
