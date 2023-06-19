66bfa16daaf5ec3f6c10daf0187dc1a6de65f5ed
dphuang2 <dylan.p.huang@gmail.com>
2023-06-19 13:07:09 -0700

    use Transition instead of collapse

--- generator/konfig-next-app/src/components/DemoOutput.tsx
+++ generator/konfig-next-app/src/components/DemoOutput.tsx
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react";
 import { CellState } from "./DemoForm";
-import { Code, Collapse, Tabs, useMantineColorScheme } from "@mantine/core";
+import { Code, Tabs, Transition, useMantineColorScheme } from "@mantine/core";
 import { useEffect, useState } from "react";
 const ReactJson = dynamic(() => import("react-json-view"), {
   ssr: false,
@@ -23,57 +23,65 @@ export const DemoOutput = observer(({ cell }: { cell: CellState }) => {
   }, [cell.jsonOutput, cell.tableOutput, haveClickedTab]);
   const { colorScheme } = useMantineColorScheme();
   return (
-    <Collapse in={cell.show}>
-      <Tabs
-        value={activeTab}
-        onTabChange={(tab) => {
-          setHaveClickedTab(true);
-          setActiveTab(tab);
-        }}
-      >
-        <Tabs.List>
-          <Tabs.Tab value="raw">Raw</Tabs.Tab>
-          <Tabs.Tab disabled={cell.jsonOutput === null} value="json">
-            JSON
-          </Tabs.Tab>
-          <Tabs.Tab disabled={cell.tableOutput === null} value="table">
-            Table
-          </Tabs.Tab>
-        </Tabs.List>
+    <Transition
+      mounted={cell.show}
+      transition="pop"
+      duration={400}
+      timingFunction="ease"
+    >
+      {(styles) => (
+        <Tabs
+          value={activeTab}
+          style={styles}
+          onTabChange={(tab) => {
+            setHaveClickedTab(true);
+            setActiveTab(tab);
+          }}
+        >
+          <Tabs.List>
+            <Tabs.Tab value="raw">Raw</Tabs.Tab>
+            <Tabs.Tab disabled={cell.jsonOutput === null} value="json">
+              JSON
+            </Tabs.Tab>
+            <Tabs.Tab disabled={cell.tableOutput === null} value="table">
+              Table
+            </Tabs.Tab>
+          </Tabs.List>
 
-        <Tabs.Panel value="raw">
-          <Code mah={500} sx={{ overflowY: "scroll" }} color="gray" block>
-            {cell.processedOutput}
-          </Code>
-        </Tabs.Panel>
+          <Tabs.Panel value="raw">
+            <Code mah={500} sx={{ overflowY: "scroll" }} color="gray" block>
+              {cell.processedOutput}
+            </Code>
+          </Tabs.Panel>
 
-        <Tabs.Panel value="json">
-          {cell.jsonOutput && (
-            <ReactJson
-              collapsed={1}
-              displayObjectSize
-              displayDataTypes={false}
-              name={false}
-              theme={colorScheme === "dark" ? "tomorrow" : undefined}
-              style={{
-                fontSize: "0.85rem",
-                padding: "0.75rem",
-                maxHeight: "500px",
-                overflowY: "scroll",
-              }}
-              src={cell.jsonOutput}
-            />
-          )}
-        </Tabs.Panel>
-        <Tabs.Panel pt="xs" value="table">
-          {cell.tableOutput && (
-            <DemoTable
-              data={cell.tableOutput.data}
-              columns={cell.tableOutput.columns}
-            />
-          )}
-        </Tabs.Panel>
-      </Tabs>
-    </Collapse>
+          <Tabs.Panel value="json">
+            {cell.jsonOutput && (
+              <ReactJson
+                collapsed={1}
+                displayObjectSize
+                displayDataTypes={false}
+                name={false}
+                theme={colorScheme === "dark" ? "tomorrow" : undefined}
+                style={{
+                  fontSize: "0.85rem",
+                  padding: "0.75rem",
+                  maxHeight: "500px",
+                  overflowY: "scroll",
+                }}
+                src={cell.jsonOutput}
+              />
+            )}
+          </Tabs.Panel>
+          <Tabs.Panel pt="xs" value="table">
+            {cell.tableOutput && (
+              <DemoTable
+                data={cell.tableOutput.data}
+                columns={cell.tableOutput.columns}
+              />
+            )}
+          </Tabs.Panel>
+        </Tabs>
+      )}
+    </Transition>
   );
 });
