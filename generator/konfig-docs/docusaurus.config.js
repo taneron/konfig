ea1bc6ace32a6a5636d8bf6faa8b145856a2498d
dphuang2 <dylan.p.huang@gmail.com>
2023-06-24 18:15:27 -0700

    reset docs

--- generator/konfig-docs/docusaurus.config.js
+++ generator/konfig-docs/docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: "/docs",
+          path: "/",
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
