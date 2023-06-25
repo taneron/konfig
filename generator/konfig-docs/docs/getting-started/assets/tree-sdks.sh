a2e51dd7194413513b668d20c5e253a212f36b5d
dphuang2 <dylan.p.huang@gmail.com>
2023-06-25 14:27:34 -0700

    finish getting-started

---
+++ generator/konfig-docs/docs/getting-started/assets/tree-sdks.sh
@@ -0,0 +1,42 @@
+/my-sdks
+❯ tree -L 2
+.
+├── LICENSE
+├── README.md
+├── flows
+│   ├── python
+│   └── typescript
+├── konfig.yaml
+├── petstore.yaml
+├── python
+│   ├── LICENSE
+│   ├── README.md
+│   ├── docs
+│   ├── petstore_client
+│   ├── poetry.lock
+│   ├── pyproject.toml
+│   ├── setup.cfg
+│   ├── setup.py
+│   └── test
+└── typescript
+    ├── README.md
+    ├── api
+    ├── api.ts
+    ├── base.ts
+    ├── client-custom.ts
+    ├── client.ts
+    ├── common.ts
+    ├── configuration.ts
+    ├── docs
+    ├── index.ts
+    ├── jest.config.ts
+    ├── models
+    ├── package.json
+    ├── pagination
+    ├── requestAfterHook.ts
+    ├── requestBeforeHook.ts
+    ├── tsconfig.json
+    ├── tsconfig.test.json
+    └── yarn.lock
+
+12 directories, 25 files
---
+++ generator/konfig-docs/docs/talk-with-us.md
@@ -0,0 +1,11 @@
+---
+---
+
+---
+
+### Talk with us
+
+If you are interested in generating SDKs with Konfig we would love to [setup a
+time](/schedule-demo) to talk. In the call we will learn about your particular
+requirements and answer any questions you might have. Typically it only takes a
+couple hours to get high quality SDKs after the conversation.
