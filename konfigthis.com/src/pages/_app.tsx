2430c7d99b1263a76fec08f71ba541d1e12b2302
dphuang2 <dylan.p.huang@gmail.com>
2023-06-25 16:59:56 -0700

    add google analytics to konfigthis.com

--- konfigthis.com/src/pages/_app.tsx
+++ konfigthis.com/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
+import Script from "next/script";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
@@ -14,6 +15,19 @@ export default function App(props: AppProps) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-L2Q9WBQN2D"
+      />
+      <Script id="google-analytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-L2Q9WBQN2D');
+      `}
+      </Script>
 
       <MantineProvider
         withGlobalStyles
