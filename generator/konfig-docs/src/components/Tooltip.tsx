a8134bc5c04b06a4348c03277ab3facc548f83d4
dphuang2 <dylan.p.huang@gmail.com>
2023-06-26 23:40:01 -0700

    Tooltip.tsx

---
+++ generator/konfig-docs/src/components/Tooltip.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { PropsWithChildren } from "react";
+import { v4 as uuid } from "uuid";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+
+export function Tooltip({
+  tip,
+  children,
+  text,
+}: PropsWithChildren<{ tip: string; text?: string }>) {
+  const id = uuid();
+  return (
+    <span
+      style={{ cursor: "pointer", textDecoration: "underline" }}
+      data-tooltip-id={id}
+      data-tooltip-content={tip}
+    >
+      {text ? text : children}
+      <ReactTooltip clickable style={{ opacity: 0.98 }} id={id} />
+    </span>
+  );
+}
--- generator/konfig-docs/src/theme/MDXContent/index.js
+++ generator/konfig-docs/src/theme/MDXContent/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import { MDXProvider } from "@mdx-js/react";
 import MDXComponents from "@theme/MDXComponents";
 import { Mermaid } from "@jakxz/mdx-mermaid/Mermaid";
+import { Tooltip } from "@site/src/components/Tooltip";
 
 export default function MDXContent({ children }) {
   return (
-    <MDXProvider components={{ ...MDXComponents, Mermaid }}>
+    <MDXProvider components={{ ...MDXComponents, Mermaid, Tooltip }}>
       {children}
     </MDXProvider>
   );
