fe931bc605b542b9b622d6e40b5634fe7ea9c34b
dphuang2 <dylan.p.huang@gmail.com>
2023-06-28 15:51:27 -0700

    put Admonition in global component scope for mdx

--- generator/konfig-docs/src/theme/MDXContent/index.js
+++ generator/konfig-docs/src/theme/MDXContent/index.js
@@ -3,10 +3,13 @@ import { MDXProvider } from "@mdx-js/react";
 import MDXComponents from "@theme/MDXComponents";
 import { Mermaid } from "@jakxz/mdx-mermaid/Mermaid";
 import { Tooltip } from "@site/src/components/Tooltip";
+import Admonition from "@theme/Admonition";
 
 export default function MDXContent({ children }) {
   return (
-    <MDXProvider components={{ ...MDXComponents, Mermaid, Tooltip }}>
+    <MDXProvider
+      components={{ ...MDXComponents, Mermaid, Tooltip, Admonition }}
+    >
       {children}
     </MDXProvider>
   );
