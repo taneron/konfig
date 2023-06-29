aa4b40503c625573dcfac2b2340c52000889d59a
dphuang2 <dylan.p.huang@gmail.com>
2023-06-29 09:41:10 -0700

    add flame

---
+++ generator/konfig-docs/src/components/Flame/Flame.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import FlameSvg from "./flame.svg";
+export const Flame = () => {
+  return (
+    <div style={{ height: "1.5rem", width: "1.5rem", display: "inline-block" }}>
+      <FlameSvg />
+    </div>
+  );
+};
