3784156881536bea841e0202647e21e5f55aa1b4
dphuang2 <dylan.p.huang@gmail.com>
2023-06-29 10:55:05 -0700

    auto add captions

--- generator/konfig-docs/src/components/VideoPlayer.tsx
+++ generator/konfig-docs/src/components/VideoPlayer.tsx
@@ -1,25 +1,30 @@
 import React from "react";
-import ReactPlayer from "react-player";
+import ReactPlayer, { ReactPlayerProps } from "react-player";
 
-export function VideoPlayer(props) {
+export function VideoPlayer(props: ReactPlayerProps) {
+  const config: ReactPlayerProps["config"] =
+    typeof props.url === "string"
+      ? {
+          file: {
+            tracks: [
+              {
+                kind: "captions",
+                src: props.url.replace("mov", "vtt"),
+                srcLang: "en",
+                label: "Captions",
+                default: true,
+              },
+            ],
+          },
+        }
+      : {};
   return (
-    <div
-      style={{
-        position: "relative",
-        paddingTop: "56.25%",
-      }}
-    >
-      <ReactPlayer
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-        }}
-        width="100%"
-        height="100%"
-        controls
-        {...props}
-      />
-    </div>
+    <ReactPlayer
+      width="100%"
+      height="100%"
+      controls
+      {...props}
+      config={config}
+    />
   );
 }
