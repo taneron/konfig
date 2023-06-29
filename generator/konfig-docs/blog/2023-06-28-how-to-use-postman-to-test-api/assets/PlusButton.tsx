6c9593a9dc28e722c7d7ef237d8bd0ab563452cb
dphuang2 <dylan.p.huang@gmail.com>
2023-06-29 10:57:08 -0700

    fix console warning

--- generator/konfig-docs/blog/2023-06-28-how-to-use-postman-to-test-api/assets/PlusButton.tsx
+++ generator/konfig-docs/blog/2023-06-28-how-to-use-postman-to-test-api/assets/PlusButton.tsx
@@ -3,8 +3,8 @@ import createCollectionButton from "./create-collection-button.png";
 
 export function PlusButton() {
   return (
-    <div style={{ width: "1rem", height: "1rem", display: "inline-block" }}>
+    <span style={{ width: "1rem", height: "1rem", display: "inline-block" }}>
       <img src={createCollectionButton} />
-    </div>
+    </span>
   );
 }
