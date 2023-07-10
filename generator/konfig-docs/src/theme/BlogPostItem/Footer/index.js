fa9be29d5647f2a21e54699f9b59da9942044c1a
dphuang2 <dylan.p.huang@gmail.com>
2023-07-10 09:49:19 -0700

    only override footer of BlogPostItem

--- generator/konfig-docs/src/theme/BlogPostItem/Footer/index.js
+++ generator/konfig-docs/src/theme/BlogPostItem/Footer/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import clsx from 'clsx';
-import {useBlogPost} from '@docusaurus/theme-common/internal';
-import EditThisPage from '@theme/EditThisPage';
-import TagsListInline from '@theme/TagsListInline';
-import ReadMoreLink from '@theme/BlogPostItem/Footer/ReadMoreLink';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import { useBlogPost } from "@docusaurus/theme-common/internal";
+import EditThisPage from "@theme/EditThisPage";
+import TagsListInline from "@theme/TagsListInline";
+import ReadMoreLink from "@theme/BlogPostItem/Footer/ReadMoreLink";
+import styles from "./styles.module.css";
 export default function BlogPostItemFooter() {
-  const {metadata, isBlogPostPage} = useBlogPost();
-  const {tags, title, editUrl, hasTruncateMarker} = metadata;
+  const {
+    metadata,
+    isBlogPostPage,
+    metadata: { authors },
+  } = useBlogPost();
+  const { tags, title, editUrl, hasTruncateMarker } = metadata;
   // A post is truncated if it's in the "list view" and it has a truncate marker
   const truncatedPost = !isBlogPostPage && hasTruncateMarker;
   const tagsExists = tags.length > 0;
@@ -18,11 +22,12 @@ export default function BlogPostItemFooter() {
   return (
     <footer
       className={clsx(
-        'row docusaurus-mt-lg',
-        isBlogPostPage && styles.blogPostFooterDetailsFull,
-      )}>
+        "row docusaurus-mt-lg",
+        isBlogPostPage && styles.blogPostFooterDetailsFull
+      )}
+    >
       {tagsExists && (
-        <div className={clsx('col', {'col--9': truncatedPost})}>
+        <div className={clsx("col", { "col--9": truncatedPost })}>
           <TagsListInline tags={tags} />
         </div>
       )}
@@ -35,12 +40,38 @@ export default function BlogPostItemFooter() {
 
       {truncatedPost && (
         <div
-          className={clsx('col text--right', {
-            'col--3': tagsExists,
-          })}>
+          className={clsx("col text--right", {
+            "col--3": tagsExists,
+          })}
+        >
           <ReadMoreLink blogPostTitle={title} to={metadata.permalink} />
         </div>
       )}
+
+      {isBlogPostPage &&
+        authors.map(({ key, name, github, linkedin, imageURL, bio }) => {
+          return (
+            <div key={key} className="p-4 mt-5 flex flex-row gap-5">
+              <img className="rounded-full h-16" src={imageURL} alt={name} />
+              <div>
+                <div className="flex flex-row gap-2 items-center mb-2">
+                  <span className="font-semibold">{name}</span>
+                  {github && (
+                    <a className="text-xs" href={github} target="_blank">
+                      GitHub
+                    </a>
+                  )}
+                  {linkedin && (
+                    <a className="text-xs" href={linkedin} target="_blank">
+                      LinkedIn
+                    </a>
+                  )}
+                </div>
+                <p className="text-sm">{bio}</p>
+              </div>
+            </div>
+          );
+        })}
     </footer>
   );
 }
