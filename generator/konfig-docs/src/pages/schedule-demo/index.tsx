4605105fc19f4b77cc542f680b9d52c6812f90a3
dphuang2 <dylan.p.huang@gmail.com>
2023-07-09 18:26:49 -0700

    add title / description to schedule a demo

--- generator/konfig-docs/src/pages/schedule-demo/index.tsx
+++ generator/konfig-docs/src/pages/schedule-demo/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import { InlineWidget } from "react-calendly";
 
 export default function ScheduleDemo() {
@@ -9,6 +8,19 @@ export default function ScheduleDemo() {
       title={`Schedule Demo`}
       description="Learn more about Konfig through a live Demo"
     >
+      <div className="container pt-6">
+        <h1>Schedule a Demo</h1>
+        <p>
+          In this demo we will:
+          <ul>
+            <li>learn about your API onboarding problems </li>
+            <li>understand your development workflow</li>
+            <li>
+              give you an overview of what it looks like to operate with Konfig
+            </li>
+          </ul>
+        </p>
+      </div>
       <div style={{ height: "700px" }}>
         <InlineWidget
           styles={{ height: "100%" }}
