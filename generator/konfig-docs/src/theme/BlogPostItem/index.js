fb5378d5487a4cb44f1b1b59ad7ef5e0c543c3b7
dphuang2 <dylan.p.huang@gmail.com>
2023-07-09 23:15:02 -0700

    swizzled and added author section

--- generator/konfig-docs/src/theme/BlogPostItem/index.js
+++ generator/konfig-docs/src/theme/BlogPostItem/index.js
@@ -1,28 +1,54 @@
 import React from "react";
-import BlogPostItem from "@theme-original/BlogPostItem";
+import clsx from "clsx";
 import { useBlogPost } from "@docusaurus/theme-common/internal";
+import BlogPostItemContainer from "@theme/BlogPostItem/Container";
+import BlogPostItemHeader from "@theme/BlogPostItem/Header";
+import BlogPostItemContent from "@theme/BlogPostItem/Content";
+import BlogPostItemFooter from "@theme/BlogPostItem/Footer";
+// apply a bottom margin in list view
+function useContainerClassName() {
+  const { isBlogPostPage } = useBlogPost();
+  return !isBlogPostPage ? "margin-bottom--xl" : undefined;
+}
+function useAuthors() {
+  const {
+    metadata: { authors },
+  } = useBlogPost();
+  return authors;
+}
 
-export default function BlogPostItemWrapper(props) {
-  const test = useBlogPost();
-  console.log(test);
+export default function BlogPostItem({ children, className }) {
+  const containerClassName = useContainerClassName();
+  const authors = useAuthors();
+  console.log(authors);
   return (
-    <>
-      <BlogPostItem {...props} />
-      <div className="p-4 mt-5 flex flex-row gap-5">
-        <img
-          className="rounded-full h-16"
-          src="/img/dylan.jpeg"
-          alt="Dylan Huang"
-        />
-        <div>
-          <h5>Dylan Huang</h5>
-          <p className="text-sm">
-            Dylan is a Co-Founder of Konfig and passionate software nerd. He
-            built API tooling at C3.ai and a viral web application with over
-            210,000 unique users.
-          </p>
-        </div>
-      </div>
-    </>
+    <BlogPostItemContainer className={clsx(containerClassName, className)}>
+      <BlogPostItemHeader />
+      <BlogPostItemContent>{children}</BlogPostItemContent>
+      <BlogPostItemFooter />
+      {authors.map(({ key, name, github, linkedin, imageURL, bio }) => {
+        return (
+          <div key={key} className="p-4 mt-5 flex flex-row gap-5">
+            <img className="rounded-full h-16" src={imageURL} alt={name} />
+            <div>
+              <div className="flex flex-row gap-2 items-center mb-2">
+                <span className="font-semibold">{name}</span>
+                {github && (
+                  <a className="text-xs" href={github} target="_blank">
+                    GitHub
+                  </a>
+                )}
+                {linkedin && (
+                  <a className="text-xs" href={linkedin} target="_blank">
+                    LinkedIn
+                  </a>
+                )}
+              </div>
+              <p className="text-sm">{bio}</p>
+            </div>
+          </div>
+        );
+      })}
+    </BlogPostItemContainer>
   );
 }
