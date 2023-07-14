8dd2d1f3ac3ea7f662d84e6325ec396a0d0b2d96
dphuang2 <dylan.p.huang@gmail.com>
2023-07-13 23:29:22 -0700

    fix build

--- konfigthis.com/src/components/Quotes/Quotes.tsx
+++ konfigthis.com/src/components/Quotes/Quotes.tsx
@@ -12,7 +12,7 @@ export const Quotes = () => {
     <Container my={rem(150)} size="lg">
       <div style={{ textAlign: "center" }}>
         <Title order={2}>Loved by growing API teams</Title>
-        <Text c="dimmed">Don't just take our word for it.</Text>
+        <Text c="dimmed">{"Don't just take our word for it."}</Text>
       </div>
       <SimpleGrid
         my={rem(50)}
@@ -23,20 +23,20 @@ export const Quotes = () => {
           color="dark"
           cite="- Peter Hayes, Co-Founder & CTO, Humanloop"
         >
-          SDK maintenance is time consuming, especially across multiple
+          {`SDK maintenance is time consuming, especially across multiple
           languages, and slows down our product delivery. Konfig help removes
-          this pain point in a way that doesn’t require behaviour change on our
+          this pain point in a way that doesn't require behaviour change on our
           part—using our existing OpenAPI spec and integrating seamlessly into
-          our existing CI/CD.
+          our existing CI/CD.`}
         </Blockquote>
         <Blockquote
           color="dark"
           cite="- Alex Sutherland, Back End Developer, SnapTrade"
         >
-          A lot of the SDK's are in languages that our team is not super well
+          {`A lot of the SDK's are in languages that our team is not super well
           versed in. In order to write these by hand we would have to learn the
           language syntax and spend hours compiling, debugging and reworking
-          code. This process is so much easier and faster.
+          code. This process is so much easier and faster.`}
         </Blockquote>
       </SimpleGrid>
     </Container>
