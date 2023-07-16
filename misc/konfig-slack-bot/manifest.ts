f6bc23df0feaa3a4d0898baedcc599ee55f0df9d
dphuang2 <dylan.p.huang@gmail.com>
2023-07-15 20:13:53 -0700

    update logo

--- misc/konfig-slack-bot/manifest.ts
+++ misc/konfig-slack-bot/manifest.ts
@@ -1,6 +1,6 @@
 import { Manifest } from "deno-slack-sdk/mod.ts";
-import SampleWorkflow from "./workflows/sample_workflow.js";
-import SampleObjectDatastore from "./datastores/sample_datastore.js";
+import SampleWorkflow from "./workflows/sample_workflow.ts";
+import SampleObjectDatastore from "./datastores/sample_datastore.ts";
 
 /**
  * The app manifest contains the app's configuration. This
@@ -9,8 +9,8 @@ import SampleObjectDatastore from "./datastores/sample_datastore.js";
  */
 export default Manifest({
   name: "konfig-bot",
-  description: "A template for building Slack apps with Deno",
-  icon: "assets/default_new_app_icon.png",
+  description: "Automation for Konfig",
+  icon: "assets/logo.png",
   workflows: [SampleWorkflow],
   outgoingDomains: [],
   datastores: [SampleObjectDatastore],
