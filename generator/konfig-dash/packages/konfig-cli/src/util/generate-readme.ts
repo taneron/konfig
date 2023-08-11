d526d526d9dec886dc76d17181b482557db87b7d
dphuang2 <dylan.p.huang@gmail.com>
2023-08-11 12:20:39 -0700

    update top-level README instructions

--- generator/konfig-dash/packages/konfig-cli/src/util/generate-readme.ts
+++ generator/konfig-dash/packages/konfig-cli/src/util/generate-readme.ts
@@ -85,25 +85,16 @@ export function generateReadme({
 
 ## How to regenerate SDKs
 
-1. Install [Konfig's CLI](https://www.npmjs.com/package/konfig-cli).
+1. Install [Konfig's CLI](https://www.npmjs.com/package/konfig-cli)
 
 \`\`\`bash
 ❯ npm install -g konfig-cli
 \`\`\`
 
-2. Signup for an account https://dash.konfigthis.com/signup.
-
-3. Login through the CLI with \`konfig login\`
-
+2. Generate SDKs
 \`\`\`bash
-❯ konfig login
-Logging into https://api.konfigthis.com
-Email: dylan@konfigthis.com
-Password: **************
-Successfully logged in as dylan@konfigthis.com
+❯ konfig generate
 \`\`\`
-
-4. Generate SDKs with \`konfig generate\`
 
 ### [Documentation](https://konfigthis.com/docs)
 `
