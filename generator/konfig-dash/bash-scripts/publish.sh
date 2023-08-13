a8f03771358ac09c5169cd21e1550c5eb181c4f6
dphuang2 <dylan.p.huang@gmail.com>
2023-08-13 02:17:18 -0700

    use curl instead

--- generator/konfig-dash/bash-scripts/publish.sh
+++ generator/konfig-dash/bash-scripts/publish.sh
@@ -4,10 +4,10 @@
 PACKAGE_NAME=$(node -p "require('./package.json').name")
 PACKAGE_VERSION=$(node -p "require('./package.json').version")
 
-# Check if the version exists on npm
-VERSION_EXISTS=$(npm view $PACKAGE_NAME@$PACKAGE_VERSION 2> /dev/null)
+# Check if the version exists on npm using curl and extract HTTP status code
+RESPONSE=$(curl -sL -w '"%{http_code}"' "https://registry.npmjs.org/$PACKAGE_NAME/$PACKAGE_VERSION")
 
-if [ -z "$VERSION_EXISTS" ]; then
+if [ "$RESPONSE" = "\"version not found: $PACKAGE_VERSION\"404" ]; then
   # Version doesn't exist, proceed with publishing
   yarn npm publish
 else
