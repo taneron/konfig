007e3e9ca0f91809845cb398642d5d9fda449819
dphuang2 <dylan.p.huang@gmail.com>
2023-08-13 02:55:55 -0700

    use --tolerate-republish

--- generator/konfig-dash/bash-scripts/publish.sh
+++ generator/konfig-dash/bash-scripts/publish.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-# Get the current package name and version from package.json
-PACKAGE_NAME=$(node -p "require('./package.json').name")
-PACKAGE_VERSION=$(node -p "require('./package.json').version")
-
-# Check if the version exists on npm using curl and extract HTTP status code
-RESPONSE=$(curl -sL -w "%{http_code}" -o /dev/null "https://registry.npmjs.org/$PACKAGE_NAME/$PACKAGE_VERSION")
-
-echo "RESPONSE: \"$RESPONSE\""
-
-if [[ "$RESPONSE" == "404" ]]; then
-  # Version doesn't exist, proceed with publishing
-  yarn npm publish
-else
-  echo "Version $PACKAGE_VERSION of $PACKAGE_NAME already exists on npm. Skipping publish."
-fi
+yarn npm publish --tolerate-republish
