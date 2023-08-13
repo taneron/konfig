3178ab85efad13798f8be52b7458417a114c3948
dphuang2 <dylan.p.huang@gmail.com>
2023-08-13 02:38:01 -0700

    debug

--- generator/konfig-dash/bash-scripts/publish.sh
+++ generator/konfig-dash/bash-scripts/publish.sh
@@ -7,6 +7,8 @@ PACKAGE_VERSION=$(node -p "require('./package.json').version")
 # Check if the version exists on npm using curl and extract HTTP status code
 RESPONSE=$(curl -sL -w '"%{http_code}"' "https://registry.npmjs.org/$PACKAGE_NAME/$PACKAGE_VERSION")
 
+echo "RESPONSE: \"$RESPONSE\""
+
 if [[ "$RESPONSE" == "404" ]]; then
   # Version doesn't exist, proceed with publishing
   yarn npm publish
