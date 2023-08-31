230c2fb60d74c67399d2af4c471062a4a007bf18
dphuang2 <dylan.p.huang@gmail.com>
2023-08-30 17:39:52 -0700

    set take to 200

--- generator/konfig-dash/api/src/functions/statistics/statistics.ts
+++ generator/konfig-dash/api/src/functions/statistics/statistics.ts
@@ -25,7 +25,7 @@ export const handler = async (event: APIGatewayEvent, context: Context) => {
     : null
 
   const generateConfigs = await db.generateConfig.findMany({
-    take: 500,
+    take: 200,
     orderBy: { created: 'desc' },
     select: {
       openApiSpecification: true,
