4b7a9ce32da54e92b3d94de9ddf139bf901b224f
dphuang2 <dylan.p.huang@gmail.com>
2023-09-15 17:36:55 -0700

    remove rewrite configuration

--- generator/konfig-next-app/next.config.js
+++ generator/konfig-next-app/next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => [
-    {
-      source: '/:path*',
-      has: [
-        {
-          type: 'host',
-          value: 'portal.konfigthis.com',
-        },
-      ],
-      destination: '/konfig-dev/acme-sdks/:path*',
-    },
-  ],
 }
 
 module.exports = nextConfig
