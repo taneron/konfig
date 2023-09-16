6581aca89c96671544e281f94b61151c004b807f
dphuang2 <dylan.p.huang@gmail.com>
2023-09-15 17:30:22 -0700

    try rewrite rule for portal.konfigthis.com

--- generator/konfig-next-app/next.config.js
+++ generator/konfig-next-app/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => [
+    {
+      source: '/:path*',
+      has: [
+        {
+          type: 'host',
+          value: 'portal.konfigthis.com',
+        },
+      ],
+      destination: '/konfig-dev/acme-sdks/:path*',
+    },
+  ],
 }
 
 module.exports = nextConfig
