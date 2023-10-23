8966305803be6240ea4f44495cbbd4018c408783
dphuang2 <dylan@konfigthis.com>
2023-10-23 14:25:57 -0700

    [ENG-1322] I Reviewed 1,000s of Opinions on GitHub Copilot (Part 2) (#307)
    * style docs
    
    * Change blog sidebar title
    
    * swizzle blog sidebar to deemphasize inactive blog post titles
    
    * fix typo
    
    ---------
    
    Co-authored-by: konfig-bot <bot@konfigthis.com>

--- generator/konfig-docs/docusaurus.config.js
+++ generator/konfig-docs/docusaurus.config.js
@@ -85,200 +85,207 @@ const beforeRemarkPluginsForPages = [
   ],
 ];
 
-
 /**
  * @returns {Promise<import('@docusaurus/types').Config>}
  */
 async function createConfig() {
-const remarkPlugins = [
-  (await import('remark-gfm')).default,
-  [
-    mermaid,
-    {
-      theme: { light: "default", dark: "dark" },
-    },
-  ],
-];
-  return {
-  title: "Konfig",
-  tagline: "Generate SDKs for your REST API",
-  url: "https://konfigthis.com",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.png",
-  themes: [
-    "docusaurus-json-schema-plugin",
+  const remarkPlugins = [
+    (await import("remark-gfm")).default,
     [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
+      mermaid,
       {
-        hashed: true,
-        indexBlog: false,
-        docsRouteBasePath: "/",
+        theme: { light: "default", dark: "dark" },
       },
     ],
-    "mdx-v2",
-  ],
-  plugins: [
-    async function docusaurus(context, options) {
-      return {
-        name: "docusaurus-tailwindcss",
-        configurePostCss(postcssOptions) {
-          // Appends TailwindCSS and AutoPrefixer.
-          postcssOptions.plugins.push(require("tailwindcss"));
-          postcssOptions.plugins.push(require("autoprefixer"));
-          return postcssOptions;
+  ];
+  return {
+    title: "Konfig",
+    tagline: "Generate SDKs for your REST API",
+    url: "https://konfigthis.com",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.png",
+    themes: [
+      "docusaurus-json-schema-plugin",
+      [
+        require.resolve("@easyops-cn/docusaurus-search-local"),
+        {
+          hashed: true,
+          indexBlog: false,
+          docsRouteBasePath: "/",
         },
-      };
+      ],
+      "mdx-v2",
+    ],
+    plugins: [
+      async function docusaurus(context, options) {
+        return {
+          name: "docusaurus-tailwindcss",
+          configurePostCss(postcssOptions) {
+            // Appends TailwindCSS and AutoPrefixer.
+            postcssOptions.plugins.push(require("tailwindcss"));
+            postcssOptions.plugins.push(require("autoprefixer"));
+            return postcssOptions;
+          },
+        };
+      },
+    ],
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    // organizationName: "facebook", // Usually your GitHub org/user name.
+    // projectName: "docusaurus", // Usually your repo name.
+
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+      defaultLocale: "en",
+      locales: ["en"],
     },
-  ],
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  // organizationName: "facebook", // Usually your GitHub org/user name.
-  // projectName: "docusaurus", // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
-
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          beforeDefaultRemarkPlugins: beforeRemarkPlugins,
-          remarkPlugins: remarkPlugins,
-          routeBasePath: "/docs",
-          sidebarPath: require.resolve("./sidebars.js"),
-        },
-        pages: {
-          beforeDefaultRemarkPlugins: beforeRemarkPluginsForPages,
-          remarkPlugins: remarkPlugins,
-        },
-        theme: {
-          customCss: [
-            require.resolve("@code-hike/mdx/styles.css"),
-            require.resolve("./src/css/custom.css"),
-          ],
-        },
-        blog: {
-          beforeDefaultRemarkPlugins: beforeRemarkPluginsForBlogs,
-          blogSidebarCount: "ALL",
-          remarkPlugins: remarkPlugins,
-          showReadingTime: true,
-          blogDescription: "Generate SDKs for your REST API",
-          truncateMarker: /{\/\* TRUNCATE \*\/}/,
-        },
-        gtag: {
-          trackingID: "G-L2Q9WBQN2D",
-          anonymizeIP: true,
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: "Konfig",
-        logo: {
-          alt: "Konfig Logo",
-          src: "img/favicon.png",
-          href: "https://konfigthis.com",
-        },
-        items: [
-          {
-            type: "doc",
-            docId: "intro",
-            position: "left",
-            label: "Documentation",
+    presets: [
+      [
+        "classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            beforeDefaultRemarkPlugins: beforeRemarkPlugins,
+            remarkPlugins: remarkPlugins,
+            routeBasePath: "/docs",
+            sidebarPath: require.resolve("./sidebars.js"),
           },
-          {
-            to: "/interactive-tutorial",
-            label: "Interactive Tutorial",
-            position: "left",
+          pages: {
+            beforeDefaultRemarkPlugins: beforeRemarkPluginsForPages,
+            remarkPlugins: remarkPlugins,
           },
-          { to: "/schedule-demo", label: "Schedule Demo", position: "right" },
-          { to: "/blog", label: "Blog", position: "right" },
-          { to: "/about", label: "About", position: "right" },
-        ],
-      },
-      api: {
-        authPersistance: "localStorage",
-        serverVariablesPersistance: "localStorage",
-      },
-      footer: {
-        style: "light",
-        links: [
-          {
-            title: "Links",
-            items: [
-              {
-                label: "Website",
-                to: "https://konfigthis.com",
-              },
-              {
-                label: "LinkedIn",
-                to: "https://www.linkedin.com/company/konfig",
-              },
+          theme: {
+            customCss: [
+              require.resolve("@code-hike/mdx/styles.css"),
+              require.resolve("./src/css/custom.css"),
             ],
           },
-          // {
-          //   title: "Docs",
-          //   items: [
-          //     {
-          //       label: "Introduction",
-          //       to: "/docs/intro",
-          //     },
-          //   ],
-          // },
-          // {
-          //   title: "Community",
-          //   items: [
-          //     {
-          //       label: "Stack Overflow",
-          //       href: "https://stackoverflow.com/questions/tagged/docusaurus",
-          //     },
-          //     {
-          //       label: "Discord",
-          //       href: "https://discordapp.com/invite/docusaurus",
-          //     },
-          //     {
-          //       label: "Twitter",
-          //       href: "https://twitter.com/docusaurus",
-          //     },
-          //   ],
-          // },
-          // {
-          //   title: "More",
-          //   items: [
-          //     {
-          //       label: "Blog",
-          //       to: "/blog",
-          //     },
-          //     {
-          //       label: "GitHub",
-          //       href: "https://github.com/facebook/docusaurus",
-          //     },
-          //   ],
-          // },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Konfig, Inc.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ["ruby", "csharp", "php"],
-      },
-    }),
+          blog: {
+            beforeDefaultRemarkPlugins: beforeRemarkPluginsForBlogs,
+            blogSidebarCount: "ALL",
+            blogSidebarTitle: "All posts",
+            remarkPlugins: remarkPlugins,
+            showReadingTime: true,
+            blogDescription: "Generate SDKs for your REST API",
+            truncateMarker: /{\/\* TRUNCATE \*\/}/,
+          },
+          gtag: {
+            trackingID: "G-L2Q9WBQN2D",
+            anonymizeIP: true,
+          },
+        }),
+      ],
+    ],
+
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        colorMode: {
+          defaultMode: "light",
+          disableSwitch: true,
+        },
+        navbar: {
+          logo: {
+            alt: "Konfig Logo",
+            src: "img/logo-light.png",
+            href: "https://konfigthis.com",
+          },
+          items: [
+            {
+              type: "doc",
+              docId: "intro",
+              position: "left",
+              label: "Documentation",
+            },
+            {
+              to: "/interactive-tutorial",
+              label: "Interactive Tutorial",
+              position: "left",
+            },
+            { to: "/schedule-demo", label: "Schedule Demo", position: "right" },
+            { to: "/blog", label: "Blog", position: "right" },
+            { to: "/about", label: "About", position: "right" },
+          ],
+        },
+        api: {
+          authPersistance: "localStorage",
+          serverVariablesPersistance: "localStorage",
+        },
+        footer: {
+          style: "light",
+          links: [
+            {
+              title: "Links",
+              items: [
+                {
+                  label: "Website",
+                  to: "https://konfigthis.com",
+                },
+                {
+                  label: "LinkedIn",
+                  to: "https://www.linkedin.com/company/konfig",
+                },
+                {
+                  label: "Blog",
+                  to: "/blog",
+                },
+              ],
+            },
+            // {
+            //   title: "Docs",
+            //   items: [
+            //     {
+            //       label: "Introduction",
+            //       to: "/docs/intro",
+            //     },
+            //   ],
+            // },
+            // {
+            //   title: "Community",
+            //   items: [
+            //     {
+            //       label: "Stack Overflow",
+            //       href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            //     },
+            //     {
+            //       label: "Discord",
+            //       href: "https://discordapp.com/invite/docusaurus",
+            //     },
+            //     {
+            //       label: "Twitter",
+            //       href: "https://twitter.com/docusaurus",
+            //     },
+            //   ],
+            // },
+            // {
+            //   title: "More",
+            //   items: [
+            //     {
+            //       label: "Blog",
+            //       to: "/blog",
+            //     },
+            //     {
+            //       label: "GitHub",
+            //       href: "https://github.com/facebook/docusaurus",
+            //     },
+            //   ],
+            // },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} Konfig, Inc.`,
+        },
+        prism: {
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
+          additionalLanguages: ["ruby", "csharp", "php"],
+        },
+      }),
+  };
 }
-};
 
 module.exports = createConfig;
