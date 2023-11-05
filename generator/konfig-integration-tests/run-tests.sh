4dcd56c7cdb3badf6740dc2e76756bf1343a54b5
dphuang2 <dylan.p.huang@gmail.com>
2023-11-05 00:37:50 -0700

    try in parallel

--- generator/konfig-integration-tests/run-tests.sh
+++ generator/konfig-integration-tests/run-tests.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-earthly --config earthly-config.yaml --secret NPM_TOKEN='dummy' --secret GITHUB_ACTIONS=false -P +test
+# You can use this script to run integration tests in earthly locally.
+
+# You can optionally pass in command line arguments to this script, which will be appended to the base command.
+# For example `./run-tests.sh --TEST_ARGS="--shard=1/2"` will run the tests with the `--shard=1/2` flag.
+
+# Store the base command in a variable
+base_cmd="earthly --config earthly-config.yaml --secret NPM_TOKEN='dummy' --secret GITHUB_ACTIONS=false -P +test"
+
+# Append command line arguments
+for arg in "$@"; do
+  base_cmd="$base_cmd $arg"
+done
+
+# Execute the command
+eval $base_cmd
