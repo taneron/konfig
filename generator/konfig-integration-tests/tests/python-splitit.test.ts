cb0237accf630d924443420fc7f3b19c0ee51210
Eddie Chayes <35413716+eddiechayes@users.noreply.github.com>
2024-01-31 10:51:56 -0800

    Move handwritten splitit tests to integration tests with custom mock server (#512)
    * Move handwritten splitit tests to integration tests with custom mock server
    
    * mock token server

--- generator/konfig-integration-tests/tests/python-splitit.test.ts
+++ generator/konfig-integration-tests/tests/python-splitit.test.ts
@@ -2,5 +2,27 @@ import { e2e } from "../util";
 import { test } from "vitest";
 
 test("splitit/python-splitit", async () => {
-  await e2e(4021);
+  await e2e(4021, {
+    customServer: {
+      routes: [
+        {
+          path: "/api/installmentplans/initiate",
+          method: "post",
+          response: {},
+        },
+        {
+          path: "/token",
+          method: "post",
+          response: {
+            access_token: "string",
+            token_type: "string",
+            expires_in: 0,
+            refresh_token: "string",
+            scope: "string",
+            id_token: "string",
+          },
+        },
+      ],
+    },
+  });
 });
