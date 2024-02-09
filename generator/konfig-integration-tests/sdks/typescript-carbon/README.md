813faab0a7aa5aedd45602180159605526b04353
Eddie Chayes <35413716+eddiechayes@users.noreply.github.com>
2024-02-08 16:55:56 -0800

    Add typescript-carbon integration test (#553)
    * add typescript-carbon integration test
    
    * remove python generator from konfig.yaml
    
    * add snapshot

---
+++ generator/konfig-integration-tests/sdks/typescript-carbon/README.md
@@ -0,0 +1,5 @@
+# konfig
+
+|Language|Version|Package Manager|README|Source|
+|-|-|-|-|-|
+|TypeScript|0.1.1|[npm](https://www.npmjs.com/package/carbon-typescript-sdk/v/0.1.1)|[README](https://github.com/konfig-dev/carbon-sdks/tree/main/typescript#readme)|[Source](https://github.com/konfig-dev/carbon-sdks/tree/main/typescript)|
---
+++ generator/konfig-integration-tests/sdks/typescript-carbon/typescript/index.test.ts
@@ -0,0 +1,51 @@
+import { Carbon } from "./index";
+import * as fs from "fs";
+
+describe("carbon-typescript-sdk", () => {
+  it("initialize client", async () => {
+    const carbon = new Carbon({
+      apiKey: "API_KEY",
+      customerId: "CUSTOMER_ID",
+    });
+    expect(carbon).not.toBeNull();
+  });
+  const carbon = new Carbon({
+    apiKey: "API_KEY",
+    customerId: "CUSTOMER_ID",
+    basePath: "http://127.0.0.1:4094",
+  });
+  it("get access token", async () => {
+    const token = await carbon.auth.getAccessToken();
+    expect(token).not.toBeNull();
+    console.log(token.data);
+    const carbonClientWithToken = new Carbon({
+      accessToken: token.data.access_token,
+      basePath: "http://127.0.0.1:4094",
+    });
+    expect(carbonClientWithToken).not.toBeNull();
+    const whiteLabeling = await carbonClientWithToken.auth.getWhiteLabeling();
+    expect(whiteLabeling).not.toBeNull();
+    console.log(whiteLabeling.data);
+  });
+
+  it("query user data sources", async () => {
+    const dataSources = await carbon.dataSources.queryUserDataSources({});
+    console.log(dataSources.data);
+  });
+  it("sync s3 files", async () => {
+    const syncS3Files = await carbon.integrations.syncS3Files({
+      ids: [{ id: "ID", bucket: "BUCKET" }],
+    });
+    console.log(syncS3Files.data);
+  });
+  it("health check", async () => {
+    const status = await carbon.health.check();
+    console.log(status.data);
+  });
+  it("file upload", async () => {
+    const file = await carbon.files.upload({
+      file: fs.readFileSync("README.md"),
+    });
+    console.log(file.data);
+  });
+});
