88868adb2529ba7588ef25a662f7e580b752702a
dphuang2 <dylan@konfigthis.com>
2024-04-17 01:20:32 -0700

    remove image

--- generator/konfig-docs/src/pages/enterprise/index.tsx
+++ generator/konfig-docs/src/pages/enterprise/index.tsx
@@ -9,14 +9,12 @@ export default function SdkPage() {
     <LayoutWithoutNav>
       <Head>
         <title>Konfig for Enterprise APIs</title>
-        <meta property="og:image" content="/img/konfig-sdks.png" />
         <meta property="og:title" content="Konfig for Enterprise" />
         <meta property="og:description" content={description} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@konfig" />
         <meta name="twitter:title" content="Konfig SDKs for Public APIs" />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content="/img/konfig-sdks.png" />
       </Head>
       <Index />
     </LayoutWithoutNav>
