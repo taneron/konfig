4730f6456c290130a406575fe8bc67a162b2c568
dphuang2 <dylan@konfigthis.com>
2024-04-17 01:21:00 -0700

    fix metas

--- generator/konfig-docs/src/pages/enterprise/index.tsx
+++ generator/konfig-docs/src/pages/enterprise/index.tsx
@@ -5,15 +5,16 @@ import Head from "@docusaurus/Head";
 export default function SdkPage() {
   const description =
     "With Konfig, you can run an efficient onboarding process by spending less time handholding customers.";
+  const title = "Konfig for Enterprise APIs";
   return (
     <LayoutWithoutNav>
       <Head>
-        <title>Konfig for Enterprise APIs</title>
-        <meta property="og:title" content="Konfig for Enterprise" />
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@konfig" />
-        <meta name="twitter:title" content="Konfig SDKs for Public APIs" />
+        <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
       </Head>
       <Index />
