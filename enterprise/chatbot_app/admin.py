307ce955ca391603c9c9b976bbac6994455a676e
dphuang2 <dylan@konfigthis.com>
2024-04-21 18:23:26 -0700

    add org, space, membership

--- enterprise/chatbot_app/admin.py
+++ enterprise/chatbot_app/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Space
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Space, Chat, Message, Organization, Membership, CurrentUserSpace
 
-# Register your models here.
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class SpaceInline(admin.TabularInline):
+    model = CurrentUserSpace
+    extra = 1
+    verbose_name_plural = "spaces"
+
+class MembershipInline(admin.TabularInline):
+    model = Membership
+    extra = 1
+    verbose_name_plural = "memberships"
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = [SpaceInline, MembershipInline]
+
+admin.site.register(Space)
+admin.site.register(Chat)
+admin.site.register(Message)
+admin.site.register(Organization)
+admin.site.register(Membership)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- enterprise/chatbot_app/models.py
+++ enterprise/chatbot_app/models.py
@@ -1,7 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 from datetime import datetime
 
+
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Space(models.Model):
+    organization = models.ForeignKey(
+        Organization, on_delete=models.CASCADE, related_name="spaces"
+    )
+
+
+class CurrentUserSpace(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="user_spaces"
+    )
+    space = models.OneToOneField(
+        Space, on_delete=models.CASCADE, related_name="user_spaces"
+    )
+
+
 class Chat(models.Model):
+    space = models.ForeignKey(Space, on_delete=models.CASCADE, related_name="chats")
     name = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -10,8 +35,24 @@ class Chat(models.Model):
             self.name = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
         super(Chat, self).save(*args, **kwargs)
 
+
+MEMBERSHIP_ROLES = (
+    ("owner", "Owner"),
+    ("admin", "Admin"),
+    ("user", "User"),
+)
+
+
+class Membership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="memberships")
+    organization = models.ForeignKey(
+        Organization, on_delete=models.CASCADE, related_name="memberships"
+    )
+    role = models.CharField(max_length=5, choices=MEMBERSHIP_ROLES)
+
+
 class Message(models.Model):
     user_message = models.TextField()
     bot_message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages')
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name="messages")
