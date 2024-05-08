1f7ad7839d6ce16b0d99e70ccf891ce958862dd0
dphuang2 <dylan@konfigthis.com>
2024-05-08 09:38:20 -0700

    add favicon

--- enterprise/enterprise/settings.py
+++ enterprise/enterprise/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -52,9 +56,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "enterprise.urls"
 
-LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/chat/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/chat/"
+LOGOUT_REDIRECT_URL = "/login/"
 
 TEMPLATES = [
     {
