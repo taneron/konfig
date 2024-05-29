ddda485b5c8bdcb1ccd737a753dd4042fc2bba4e
Eddie Chayes <35413716+eddiechayes@users.noreply.github.com>
2024-05-28 18:21:31 -0700

    fix for humanloop missing import bug (#703)
    * fix for humanloop missing import bug
    
    * fixed import

--- generator/konfig-generator-api/src/main/java/org/openapitools/codegen/DefaultCodegen.java
+++ generator/konfig-generator-api/src/main/java/org/openapitools/codegen/DefaultCodegen.java
@@ -53,6 +53,7 @@ import org.openapitools.codegen.languages.JavaClientCodegen;
 import org.openapitools.codegen.languages.PythonClientCodegen;
 import org.openapitools.codegen.languages.PythonPriorClientCodegen;
 import org.openapitools.codegen.languages.RustServerCodegen;
+import org.openapitools.codegen.languages.TypeScriptAxiosClientCodegen;
 import org.openapitools.codegen.meta.FeatureSet;
 import org.openapitools.codegen.meta.GeneratorMetadata;
 import org.openapitools.codegen.meta.Stability;
@@ -5339,7 +5340,8 @@ public class DefaultCodegen implements CodegenConfig {
         if (parameter.getSchema() != null) {
             parameterSchema = unaliasSchema(parameter.getSchema());
             parameterModelName = getParameterDataType(parameter, parameterSchema);
-            if (this instanceof PythonPriorClientCodegen && parameter.getSchema().get$ref() != null
+            if ((this instanceof PythonPriorClientCodegen || this instanceof TypeScriptAxiosClientCodegen)
+                    && parameter.getSchema().get$ref() != null
                     && parameterModelName != null) {
                 // This is a ref so add it to list of imports
                 imports.add(parameterModelName);
